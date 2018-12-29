98fb07405e75974cfec4d98cc6776e703272f449
coco <coco@hexgolems.com>
2018-12-29 19:58:26 +0100

    less warnings

--- src/wave.rs
+++ src/wave.rs
@@ -1,45 +1,35 @@
-use ggez::conf;
-use ggez::event;
-use ggez::graphics;
-use ggez::graphics::{DrawMode, Point2};
-use ggez::timer;
-use ggez::{Context, GameResult};
-use std::collections::HashMap;
-use std::env;
-use std::path;
-
 use crate::game_state::GameState;
 use crate::enemies::Enemy;
 
 pub struct Wave {
-    spawnDelay: usize,
-    nextSpawn: usize,
-    enemyCount: usize,
+    spawn_delay: usize,
+    next_spawn: usize,
+    enemy_count: usize,
 }
 
 impl Wave {
-    pub fn new(spawnDelay:usize, enemyCount: usize) -> Self {
+    pub fn new(spawn_delay:usize, enemy_count: usize) -> Self {
         return Self {
-            spawnDelay,
-            nextSpawn: 0,
-            enemyCount,
+            spawn_delay,
+            next_spawn: 0,
+            enemy_count,
         };
     }
 
-    pub fn resetSpawn(&mut self) {
-        self.nextSpawn = self.spawnDelay
+    pub fn reset_spawn(&mut self) {
+        self.next_spawn = self.spawn_delay
     }
 
     pub fn tick(state: &mut GameState) {
-        if state.wave.nextSpawn == 0 {
-            if state.wave.enemyCount != 0 {
+        if state.wave.next_spawn == 0 {
+            if state.wave.enemy_count != 0 {
                 state.enemies
                     .spawn(Enemy::new(state.map.tile_pos(0, 3), 10.0, 0.25));
-                state.wave.enemyCount -= 1;
+                state.wave.enemy_count -= 1;
             }
-            state.wave.resetSpawn()
+            state.wave.reset_spawn()
         } else {
-            state.wave.nextSpawn -= 1;
+            state.wave.next_spawn -= 1;
         }
     }
 
