c2c264cfe9f581d82261685cc57a74e694b93479
coco <coco@hexgolems.com>
2018-12-29 15:47:06 +0100

    initial

---
+++ src/enemies.rs
@@ -0,0 +1,78 @@
+use ggez::conf;
+use ggez::event;
+use ggez::graphics;
+use ggez::graphics::{DrawMode, Point2};
+use ggez::timer;
+use ggez::{Context, GameResult};
+use std::env;
+use std::path;
+use std::collections::HashMap;
+
+#[derive(Eq,PartialEq,Hash,Copy,Clone)]
+enum Display{
+    Zombie,
+}
+use self::Display::*;
+
+pub struct Enemy{
+    disp: Display,
+    position: graphics::Point2,
+    health: f32,
+}
+
+impl Enemy {
+    pub fn new(position: graphics::Point2, health: f32) -> Self {
+        return Self{disp: Zombie, position, health}
+    }
+}
+
+pub struct Enemies{
+    enemies: Vec<Enemy>,
+    images: HashMap<Display, graphics::Image>,
+}
+
+impl Enemies{
+
+    fn load_img(&mut self, ctx: &mut Context, disp: Display, path: &str) -> GameResult<()>{
+        let mut img = graphics::Image::new(ctx, path)?;
+        img.set_filter(graphics::FilterMode::Nearest);
+        self.images.insert(disp, img);
+        return Ok(());
+    }
+
+    pub fn new() -> Self{
+        let enemies = vec!();
+        let images  = HashMap::new();
+        return Self{enemies, images}
+    }
+
+    pub fn init(&mut self, ctx: &mut Context) -> GameResult<()>{
+        self.load_img(ctx, Zombie, "/enemy.png")?;
+        return Ok(());
+    }
+
+    pub fn spawn(&mut self, enemy: Enemy){
+        self.enemies.push(enemy);
+    }
+
+
+    pub fn draw(&self, ctx: &mut Context) -> GameResult<()> {
+        for e in self.enemies.iter() {
+            graphics::draw_ex(
+                ctx,
+                &self.images[&e.disp],
+                graphics::DrawParam {
+                    // src: src,
+                    dest: e.position,
+                    //rotation: self.zoomlevel,
+                    // offset: Point2::new(-16.0, 0.0),
+                    scale: Point2::new(4.0,4.0),
+                    // shear: shear,
+                    ..Default::default()
+                },
+            )?;
+        }
+        Ok(())
+    }
+
+}
---
+++ src/main.rs
@@ -0,0 +1,83 @@
+extern crate ggez;
+use ggez::conf;
+use ggez::event;
+use ggez::graphics;
+use ggez::graphics::{DrawMode, Point2};
+use ggez::timer;
+use ggez::{Context, GameResult};
+use std::env;
+use std::path;
+use std::collections::HashMap;
+
+mod enemies;
+mod map;
+use self::map::GameMap;
+use self::enemies::Enemies;
+use self::enemies::Enemy;
+
+struct MainState {
+    map: GameMap,
+    enemies: Enemies,
+}
+
+impl MainState {
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        let mut map = GameMap::new();
+        map.init(ctx);
+        let mut enemies = Enemies::new();
+        enemies.init(ctx);
+        let s = MainState {
+            map,
+            enemies,
+        };
+        Ok(s)
+    }
+
+    pub fn spawn(&mut self){
+        self.enemies.spawn( Enemy::new(self.map.tile_pos(0,3), 10.0) )
+    }
+}
+
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        const DESIRED_FPS: u32 = 60;
+
+        while timer::check_update_time(ctx, DESIRED_FPS) {
+            //self.zoomlevel += 0.01;
+        }
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx);
+        graphics::set_color(ctx, graphics::WHITE)?;
+        self.map.draw(ctx);
+        self.enemies.draw(ctx);
+
+        graphics::present(ctx);
+        Ok(())
+    }
+}
+
+pub fn main() {
+    let c = conf::Conf::new();
+    let ctx = &mut Context::load_from_conf("drawing", "ggez", c).unwrap();
+
+    // We add the CARGO_MANIFEST_DIR/resources do the filesystems paths so
+    // we we look in the cargo project for files.
+    if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+        let mut path = path::PathBuf::from(manifest_dir);
+        path.push("resources");
+        ctx.filesystem.mount(&path, true);
+    }
+
+    println!("{}", graphics::get_renderer_info(ctx).unwrap());
+    let state = &mut MainState::new(ctx).unwrap();
+    state.spawn();
+    if let Err(e) = event::run(ctx, state) {
+        println!("Error encountered: {}", e);
+    } else {
+        println!("Game exited cleanly.");
+    }
+}
