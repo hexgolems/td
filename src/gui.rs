56c364da29b8f5c7c496ad93aec0e4438fe55d6c
coco <coco@hexgolems.com>
2018-12-29 17:46:59 +0100

    added cursor movement

--- src/gui.rs
+++ src/gui.rs
@@ -1,18 +1,24 @@
 use ggez::conf;
-use ggez::event;
-use ggez::graphics;
-use ggez::graphics::{DrawMode, Point2};
+use ggez::event::{self, Keycode, Mod};
+use ggez::graphics::{self, DrawMode, Point2};
 use ggez::timer;
 use ggez::{Context, GameResult};
 use std::collections::HashMap;
 use std::env;
 use std::path;
 
+use crate::game_state::GameState;
+use crate::towers::{Tower, Towers};
+
 #[derive(Eq, PartialEq, Hash, Copy, Clone)]
 enum Images {
     Cursor,
 }
 use self::Images::*;
+
+enum CursorMode {
+    BuildCannon,
+}
 
 pub struct Gui {
     cursor_pos: graphics::Point2,
@@ -48,13 +54,33 @@ impl Gui {
                 // src: src,
                 dest: self.cursor_pos,
                 //rotation: self.zoomlevel,
-                // offset: Point2::new(-16.0, 0.0),
+                offset: Point2::new(1.0 / 22.0, 1.0 / 22.0),
                 scale: Point2::new(4.0, 4.0),
                 // shear: shear,
                 ..Default::default()
             },
         )?;
         Ok(())
+    }
+
+    pub fn key_down(state: &mut GameState, keycode: Keycode, _keymod: Mod, _repeat: bool) {
+        if keycode == Keycode::Up {
+            state.gui.cursor_pos.y -= 4.0 * 20.0;
+        }
+        if keycode == Keycode::Down {
+            state.gui.cursor_pos.y += 4.0 * 20.0;
+        }
+        if keycode == Keycode::Left {
+            state.gui.cursor_pos.x -= 4.0 * 20.0;
+        }
+        if keycode == Keycode::Right {
+            state.gui.cursor_pos.x += 4.0 * 20.0;
+        }
+        if keycode == Keycode::Space {
+            state
+                .towers
+                .spawn(Tower::new(state.gui.cursor_pos, 1.0, 1.0, 0.5));
+        }
     }
 
     pub fn tick(&mut self) {}
